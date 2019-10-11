58d88e8fe7a5a26846a452ac059f6dfd7df4bae6
DonggunJung <donggun.dev@gmail.com>
2019-10-11 16:20:19 +0900

    update

---
+++ client/components/upload/Upload.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Upload = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default Upload
---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import DashBard from '../components/dashboard/Dashboard'
+
+export default function Index() {
+  return (
+    <Container >
+        <DashBard />
+    </Container>
+  );
+}
